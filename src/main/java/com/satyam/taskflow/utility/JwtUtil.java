7d17e7fbb66fbbc380311331a71c3725a7a1a6d0
satyamsingh2003 <satyamischill@gmail.com>
2025-06-19 16:01:49 +0530

    Added JWT Utility Class to Generate/Validate Token

---
+++ src/main/java/com/satyam/taskflow/utility/JwtUtil.java
@@ -0,0 +1,42 @@
+package com.satyam.taskflow.utility;
+
+import java.util.Date;
+
+//import javax.crypto.SecretKey;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+//import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+private final String jwtSecret = "SnowBall";
+private final int jwtExpirationMs = 86400000; // 1day 
+//private final SecretKey secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+
+public String generateToken(UserDetails userDetails) {
+	return Jwts.builder()
+			.setSubject(userDetails.getUsername())
+			.setIssuedAt(new Date())
+			.setExpiration(new Date(System.currentTimeMillis()+jwtExpirationMs))
+			.signWith(SignatureAlgorithm.HS512, jwtSecret)
+			.compact();
+}
+
+public String extractUsername(String token) {
+	return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+}
+
+public boolean validateToken(String token, UserDetails userDetails) {
+	String username = extractUsername(token);
+	return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+}
+
+public boolean isTokenExpired(String token) {
+	Date expiration = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getExpiration();
+	return expiration.before(new Date()); // returns true if it is expired 
+}
+}
