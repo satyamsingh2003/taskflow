30210ebbcfed2b65c705ff8795059ac244b0b76e
satyamsingh2003 <satyamischill@gmail.com>
2025-06-19 17:25:50 +0530

    Added login authentication logic in AuthController Class

--- src/main/java/com/satyam/taskflow/controller/auth/AuthController.java
+++ src/main/java/com/satyam/taskflow/controller/auth/AuthController.java
@@ -3,13 +3,20 @@ package com.satyam.taskflow.controller.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.satyam.taskflow.dto.LoginRequest;
+import com.satyam.taskflow.dto.LoginResponse;
 import com.satyam.taskflow.dto.RegisterRequest;
 import com.satyam.taskflow.entity.User;
+import com.satyam.taskflow.security.CustomUserDetailsService;
+import com.satyam.taskflow.security.JwtUtil;
 import com.satyam.taskflow.service.AuthService;
 import com.satyam.taskflow.utility.ResponseStructure;
 
@@ -17,6 +24,16 @@ import com.satyam.taskflow.utility.ResponseStructure;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private CustomUserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtUtil jwtUtil;
+	
 @Autowired
 private AuthService authService;	
 @PostMapping("/register")
@@ -26,5 +43,18 @@ public ResponseEntity<ResponseStructure<User>> register(@RequestBody RegisterReq
 	rs.setMessage("User Object Created Successfully");
 	rs.setData(authService.Register(request));
 	return new ResponseEntity<ResponseStructure<User>>(rs,HttpStatus.ACCEPTED);
+}
+
+@PostMapping("/login")
+public ResponseEntity<?> login (@RequestBody LoginRequest request){
+	try {
+		authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+	}catch(Exception e){
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid Credentials ");
+	}
+	final UserDetails userDetails= userDetailsService.loadUserByUsername(request.getUsername());
+	final String token = jwtUtil.generateToken(userDetails);
+	return ResponseEntity.ok(new LoginResponse(token));
 }
 }
