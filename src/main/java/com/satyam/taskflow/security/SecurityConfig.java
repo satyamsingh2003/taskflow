d9a1362c0af7e2a09b8df7638b866b7ebeaebeb0
satyamsingh2003 <satyamischill@gmail.com>
2025-06-21 16:21:51 +0530

    Added Dtos , JwtUtil for Generate/Validate Token , UserDetailService ,wrote logic for login created JwtFilter to process Incoming Tokens , Added Authentication Manager Bean in Security Config

--- src/main/java/com/satyam/taskflow/security/SecurityConfig.java
+++ src/main/java/com/satyam/taskflow/security/SecurityConfig.java
@@ -1,28 +1,46 @@
 package com.satyam.taskflow.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+//	@Autowired
+//	private CustomUserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtFilter jwtFilter;
+	
     @Bean
      PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
     }
+    
     @Bean
-     SecurityFilterChain filter(HttpSecurity http) throws Exception{
-    	return http.csrf(csrf->csrf.disable())
+     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    			http.csrf(csrf->csrf.disable())
     			.authorizeHttpRequests(auth->auth.requestMatchers("/auth/**")
     			.permitAll()
     			.anyRequest().authenticated())
-				.sessionManagement(sess->sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.build();
+				.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+				http.addFilterBefore(jwtFilter,UsernamePasswordAuthenticationFilter.class);
+				return http.build();
+    }
+        
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+    	return config.getAuthenticationManager();
     }
 }
